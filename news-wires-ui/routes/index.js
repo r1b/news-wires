f4d663c6b993d029615d9acdb452958f3a3e6759
Robert C Jensen <rob@computerlab.io>
2017-06-04 20:12:10 -0400

    feature: adds basic UI

---
+++ news-wires-ui/routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var models = require('news-wires-db');
+var router = express.Router();
+
+const NEWS_ITEMS_PER_PAGE = 30
+
+router.get('/:page?', function(req, res, next) {
+  let page;
+
+  if (!req.params.page) {
+    page = 1;
+  }
+  else {
+    page = parseInt(req.params.page);
+  }
+
+  if (isNaN(page)) {
+    res.render('error', { message: "Error", error: new Error('Invalid page') });
+  }
+  else {
+    models.NewsItem.findAll({
+      include: [
+        { model: models.NewsSource }
+      ],
+      limit: NEWS_ITEMS_PER_PAGE,
+      offset: NEWS_ITEMS_PER_PAGE * (page - 1),
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    }).then((newsItems) => {
+      res.render('index', { newsItems: newsItems, page: page, title: 'news-wires'});
+    });
+  }
+});
+
+module.exports = router;
