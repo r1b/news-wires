3819137a56c11291c674fb07b53733fa195b0f70
Robert C Jensen <rob@computerlab.io>
2017-06-15 22:10:57 -0400

    bugfix: fixes bugs in migration

--- news-wires-db/data-migrations/20170614-locale.sql
+++ news-wires-db/data-migrations/20170614-locale.sql
@@ -2,13 +2,13 @@ update news_source
 set "locale" = 'en';
 
 insert into news_source values
-(DEFAULT, 'AP', 373490841, '{"apnews\\.com/[0-9a-f]{32}"}', 'es');
+(DEFAULT, 'AP', 373490841, DEFAULT, DEFAULT, '{"apnews\\.com/[0-9a-f]{32}"}', 'es');
 
 insert into news_source values
-(DEFAULT, 'REUTERS', 201867844, '{"lta\\.reuters\\.com"}', 'es');
+(DEFAULT, 'REUTERS', 201867844, DEFAULT, DEFAULT, '{"lta\\.reuters\\.com"}', 'es');
 
 insert into news_source values
-(DEFAULT, 'UPI', 430091815, '{"espanol\\.upi\\.com"}', 'es');
+(DEFAULT, 'UPI', 430091815, DEFAULT, DEFAULT, '{"espanol\\.upi\\.com"}', 'es');
 
 insert into news_source values
-(DEFAULT, 'AFP', 851108442, '{"swissinfo\\.ch","es-us\\.noticias\\.yahoo\\.com"}', 'es');
+(DEFAULT, 'AFP', 851108442, DEFAULT, DEFAULT, '{"swissinfo\\.ch","es-us\\.noticias\\.yahoo\\.com"}', 'es');
--- news-wires-db/migrations/20170614022214-name-not-unique.js
+++ news-wires-db/migrations/20170614022214-name-not-unique.js
@@ -2,32 +2,13 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.changeColumn(
-      'news_source',
-      'name',
-      {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: false,
-        validate: {
-          isUppercase: true
-        }
-      }
-    );
+    return queryInterface.removeConstraint('news_source', 'news_source_name_key');
   },
 
   down: function (queryInterface, Sequelize) {
-    return queryInterface.changeColumn(
-      'news_source',
-      'name',
-      {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isUppercase: true
-        }
-      }
-    );
+    return queryInterface.addConstraint('news_source', ['name'], {
+        type: 'unique',
+        name: 'news_source_name_key'
+    });
   }
 };
