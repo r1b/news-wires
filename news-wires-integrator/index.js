f4cbcca26aa6129e47c59176a5b947ffae531c77
Robert C Jensen <rob@computerlab.io>
2017-07-01 15:48:06 -0400

    bugfix: fixes integrator module import

--- news-wires-integrator/index.js
+++ news-wires-integrator/index.js
@@ -1,3 +1,3 @@
 module.exports = function () {
-  require('./lib/integrator');
+  require('./lib/integrator')();
 };
--- news-wires-integrator/lib/integrator.js
+++ news-wires-integrator/lib/integrator.js
@@ -1,9 +1,14 @@
 const models = require('news-wires-db')
 
 module.exports = function () {
-  models.NewsIntegration.findAll().then((newsIntegrations) => {
-    newsIntegrations.forEach((newsIntegration) => {
-      newsIntegration[newsIntegration.type]();
+  models.NewsIntegration
+    .findAll()
+    .then((newsIntegrations) => {
+      newsIntegrations.forEach((newsIntegration) => {
+        newsIntegration[newsIntegration.type]();
+      });
+    })
+    .catch((error) => {
+      console.error(error);
     });
-  });
 };
