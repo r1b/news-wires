dcf8b1b3df229c7fb91f5bba9f5b548af75b4746
Robert C Jensen <rob@computerlab.io>
2017-07-01 21:02:51 -0400

    bugfix: adds custom headline selector for XNN

--- news-wires-db/data-migrations/20170701-custom-headline.sql
+++ news-wires-db/data-migrations/20170701-custom-headline.sql
@@ -1,1 +1,3 @@
 update news_source set "headlineSelector" = '#liheading' where name = 'PTI';
+update news_source set "headlineSelector" = '#bltitle' where name = 'XNN' and locale = 'en';
+update news_source set "headlineSelector" = '#Title' where name = 'XNN' and locale = 'es';
